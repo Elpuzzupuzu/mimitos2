7404c973263a21d655b1af3c701366be288cf0da
yael <howitzer117@outlook.com>
2024-10-16 09:35:06 -0600

    terminada la logica basica del login

---
+++ views/js/login2.js
@@ -0,0 +1,32 @@
+document.getElementById('loginForm').addEventListener('submit', async function (event) {
+    event.preventDefault(); // Prevenir el envío del formulario
+
+    const user_name = document.getElementById('user_name').value; // Capturar el nombre de usuario
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('http://localhost:3000/users/login', {  // Ajusta la URL a la de tu servidor
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ user_name: user_name, password: password }) // Enviar user_name en lugar de email
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            // Guardar el token en localStorage
+            localStorage.setItem('accessToken', data.accessToken);
+
+            // Redirigir a la página principal (index.html)
+            window.location.href = './index.html';
+        } else {
+            // Mostrar el mensaje de error en la interfaz
+            alert(data.message || 'Error en las credenciales');
+        }
+    } catch (error) {
+        console.error('Error en el servidor:', error);
+        alert('Ocurrió un problema en el servidor. Intenta de nuevo más tarde.');
+    }
+});
