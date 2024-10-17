b9c3656c14295636e73ef4e631b767f46d63aa86
yael <howitzer117@outlook.com>
2024-10-17 11:09:32 -0600

    pruebas del resposive

--- config/database.js
+++ config/database.js
@@ -1,34 +1,34 @@
-      const { Sequelize } = require('sequelize');
+      // const { Sequelize } = require('sequelize');
 
-      const sequelize = new Sequelize('mimitos2', 'root', '1234', {
-        host: 'localhost',
-          dialect: 'mysql'
-      });
+      // const sequelize = new Sequelize('mimitos2', 'root', '1234', {
+      //   host: 'localhost',
+      //     dialect: 'mysql'
+      // });
 
-      module.exports = sequelize;
+      // module.exports = sequelize;
 
 // config/database.js
 
 
 
-// require('dotenv').config(); // Asegúrate de requerir dotenv
+require('dotenv').config(); // Asegúrate de requerir dotenv
 
-// const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-//   host: process.env.DB_HOST,
-//   dialect: 'mysql',
-//   port: process.env.DB_PORT,
-//   logging: false
-// });
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  port: process.env.DB_PORT,
+  logging: false
+});
 
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Conexión a la base de datos exitosa');
-//   })
-//   .catch(err => {
-//     console.error('Error al conectar a la base de datos:', err);
-//   });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos exitosa');
+  })
+  .catch(err => {
+    console.error('Error al conectar a la base de datos:', err);
+  });
 
-// module.exports = sequelize;
+module.exports = sequelize;
